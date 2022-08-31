b15255cd2e35e1e5111bc3ea67d9ef97572a8716
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-31 15:08:08 -0300

    controller created + city model

--- models/City.js
+++ models/City.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose')
 const citiesSchema = new mongoose.Schema({
     city: {type: String, required: true},
     country: {type: String, required: true},
+    featuredLocation: {type: String, required: true},
     photo: {type: String, required: true},
     fundation: {type: Date, required: true},
     description: {type: String, required: true},
+    smalldescription: {type: String, required: true},
     population: {type: Number, required: true},
 })
 
