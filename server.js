dfbc335cdf3aecec8a53e84cfa8e68a9cffbcb39
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-31 12:20:49 -0300

    update routes for server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+
+const CITY = require('./routes/cities');
 //var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,12 +25,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/cities', CITY);
+
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  //next(createError(404));
-  //res.json()
+ next(createError(404));
 });
 
 // error handler
