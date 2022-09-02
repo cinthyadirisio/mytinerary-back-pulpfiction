ace66ef19b884614199ed745907f85384c30b86c
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-02 18:47:55 -0300

    delete itinerary controller + route

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { createItinerary, upDate } = require('../controllers/itineraryController')
+const { createItinerary, upDate, deleteItinerary } = require('../controllers/itineraryController')
 
 /* Routes for controllers  */
 
 
 router.post('/itineraries', createItinerary)
 router.patch('/:id', upDate)
-
+router.delete('/:id', deleteItinerary)
 
 
 module.exports = router;
