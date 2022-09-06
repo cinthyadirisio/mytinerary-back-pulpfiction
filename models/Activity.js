0cf9d16ea7445a77930bca4ab94da02bca56d4b2
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-06 13:11:25 -0300

    models populate

--- models/Activity.js
+++ models/Activity.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const activitySchema = new mongoose.Schema({
     name: {type: String, required: true},
     photo: {type: String, required: true},
-    itinerary: {type: String, required: true}
+    itinerary: {type: mongoose.Types.ObjectId, ref:'itinerary', required: true}
 })
 
 const ACTIVITY = mongoose.model(
--- models/Comment.js
+++ models/Comment.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
     comment: {type: String, required: true},
-    user: {type: String, required: true},
-    itinerary: {type: String, required: true}
+    user: {type: mongoose.Types.ObjectId, ref:'users', required: true},
+    itinerary: {type: mongoose.Types.ObjectId, ref:'itinerary', required: true}
 })
 
 const COMMENT = mongoose.model(
