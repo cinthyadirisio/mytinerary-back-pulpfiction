2919992d47165f59682ec707da2e10f82fc9a81d
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-12 17:51:06 -0300

    sign in controller + confirmation email

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer')
-const {google} = require('googleapis')
+const { google } = require('googleapis')
 const OAuth2 = google.auth.OAuth2
-const {GOOGLE_USER, GOOGLE_ID,GOOGLE_SECRET,GOOGLE_REFRESH,GOOGLE_URL} = process.env
+const { GOOGLE_USER, GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REFRESH, GOOGLE_URL } = process.env
 
 
-const sendMail = async (mail, code) => {
+const sendMail = async (mail, code, name, photo) => {
 
     const client = new OAuth2(
         GOOGLE_ID,
@@ -22,17 +22,17 @@ const sendMail = async (mail, code) => {
 
 
 
-   const transport = nodemailer.createTransport({
+    const transport = nodemailer.createTransport({
         service: 'gmail',
-        auth:{
+        auth: {
             user: GOOGLE_USER,
             type: 'OAuth2',
-            clientId : GOOGLE_ID,
+            clientId: GOOGLE_ID,
             clientSecret: GOOGLE_SECRET,
             refreshToken: GOOGLE_REFRESH,
             accessToken: accessToken
         },
-        tls:{
+        tls: {
             rejectUnauthorized: false
         }
     })
@@ -42,17 +42,19 @@ const sendMail = async (mail, code) => {
         to: mail,
         subject: 'Verify your Pulp Fiction Account',
         html: `
-        <div>
-            <p>Mensaje de bienvenida</p>
-            <p>Hola cin</p>
-            <p>${code}</p>
-        </div>
-        ` //html puro 
+            <h1 style="text-align: center; color: #20d8ed;"><strong>
+            <img src="https://cdn.discordapp.com/attachments/1009863859537989682/1018971999898451989/verifyEmail.png" style="display: block; margin-left: auto; margin-right: auto; " width="450"  ></strong></h1>
+            <p style="text-align: center;"><strong>Pulp Fiction Travel email confirmation<br />  ${name}!  Thank you for your joining us!, we hope you find the trip of your DREAMS</strong></p>
+            <p style="text-align: center;">&nbsp;</p>
+            <p><strong><img style="display: block; margin-left: auto; margin-right: auto;" src="https://i.pinimg.com/originals/ba/4e/51/ba4e51dbabcc361e9393d32f2514624d.gif" width="543" height="330" /></strong></p>
+            <p>&nbsp;</p>
+            <p style="text-align: center;">Please click the link to verify your email.  <br/> <br/>  <a style="background: #04BF9D; color: #ffffff; padding: 10px 50px; border-radius: 3px; text-align: center;" href="https://localhost:4000/${code}">confirm</a></p>
+        `
     }
 
 
-    await transport.sendMail(mailOptions, (error,response)=>{
-        if (error){
+    await transport.sendMail(mailOptions, (error, response) => {
+        if (error) {
             console.log(error)
         } else {
             console.log("Receive ok")
