d16dc4dbba15995ac50e05910b63e445effad4b6
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-15 10:49:50 -0300

    minor update

--- routes/activity.js
+++ routes/activity.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 const { createActivity, readById } = require('../controllers/activityController')
 
-/* Routes for controllers  */
-
 
 router.post('/', createActivity)
 router.get('/', readById)
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,5 @@ router.get('/', function(req, res, next) {
   res.json();
 });
 
-// Todas las rutas =>            root
-//la ruta principal arrancar en /cities / =>  crear | /cities/:id => leer
-
-//router.use('/cities',eventRouter)
 
 module.exports = router;
