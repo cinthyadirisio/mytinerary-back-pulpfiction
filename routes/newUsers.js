6248fef37f190c14ecf6e854c8c7d9c4f4a4c1f1
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-23 14:28:12 -0300

    adde new controller and route for edit profile

--- routes/newUsers.js
+++ routes/newUsers.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const passport = require('../config/passport');
 var router = express.Router();
-const { signUp, userVerify, signIn, signOut,signInWithToken } = require('../controllers/userController')
+const { signUp, userVerify, signIn, signOut,signInWithToken, editProfile } = require('../controllers/userController')
 
 /* Routes for controllers  */
 
@@ -10,6 +10,7 @@ router.get('/verify/:code', userVerify)
 router.get('/token', passport.authenticate('jwt', {session:false}), signInWithToken)
 router.post('/signin',signIn)
 router.post('/signout', signOut)
+router.patch('/update/:id', editProfile)
 
 
 
